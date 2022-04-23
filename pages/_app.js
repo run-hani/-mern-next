07b9e8757a8ee926540068dc0bbdeeef129a7dce
run-hani <hanirun@kakao.com>
2022-04-23 13:57:53 +0900

    fix: could not find react-redux context value 에러 픽스

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,23 @@
 import {Layout} from "./common";
 
 import Head from 'next/head'
-const App = ({ Component, pageProps}) => {
-  return (<>
-    <Head>
-      <meta charSet="utf-8"/>
-      <meta name="viewport" 
-      content="width=device-width, user-scalable=no, 
+import {wrapper} from '../redux/store.ts'
+
+const App = ({Component, pageProps}) => {
+    return (
+        <> < Head > <meta charSet="utf-8"/>
+        <meta
+            name="viewport"
+            content="width=device-width, user-scalable=no,
       initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"></meta>
-      <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
-      <title>Soccer App</title>
+        <meta httpEquiv="X-UA-Compatible" content="ie=edge"/>
+        <title>Soccer App</title>
     </Head>
     <Layout>
-      <Component {...pageProps} />
+        <Component {...pageProps}/>
     </Layout>
-    </>
-  )
+</>
+    )
 }
 
-export default App
+export default wrapper.withRedux(App)
